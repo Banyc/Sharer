22943f4d9f57cdba840e5f2db52fe905c25ce6e0
Banyc <36535895+Banyc@users.noreply.github.com>
2021-04-03 16:19:53 +0800

    [bug] Make up the previous commit

--- src/SharerBlazorServer/Models/FileSliceInfoModel.cs
+++ src/SharerBlazorServer/Models/FileSliceInfoModel.cs
@@ -18,22 +18,36 @@ namespace SharerBlazorServer.Models
         public string FileSizeHumanReadable {
             get
             {
-                if (this.FileSize < 1024)
-                {
-                    return $"{this.FileSize:0} B";
-                }
-                else if (this.FileSize < 1024 * 1024)
-                {
-                    return $"{this.FileSize / 1024:0} KB";
-                }
-                else if (this.FileSize < 1024 * 1024 * 1024)
-                {
-                    return $"{(double)this.FileSize / (1024 * 1024):0.##} M";
-                }
-                else
-                {
-                    return $"{(double)this.FileSize / (1024 * 1024 * 1024):0.##} G";
-                }
+                return GetHumanReadableSize(this.FileSize);
+            }
+        }
+
+        [JsonIgnore]
+        public string EndHumanReadable
+        {
+            get
+            {
+                return GetHumanReadableSize(this.End);
+            }
+        }
+
+        private static string GetHumanReadableSize(int byteSize)
+        {
+            if (byteSize < 1024)
+            {
+                return $"{byteSize:0} B";
+            }
+            else if (byteSize < 1024 * 1024)
+            {
+                return $"{byteSize / 1024:0} KB";
+            }
+            else if (byteSize < 1024 * 1024 * 1024)
+            {
+                return $"{(double)byteSize / (1024 * 1024):0.##} M";
+            }
+            else
+            {
+                return $"{(double)byteSize / (1024 * 1024 * 1024):0.##} G";
             }
         }
     }
